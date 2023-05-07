9af72c1a56069ec5017b199d568aeb096e447c0a
JuditRoca <roca.tovar@gmail.com>
2023-05-07 18:37:14 +0200

    final

--- clases.py
+++ clases.py
@@ -1,19 +1,33 @@
-class lancha():
+
+class barco():
     def __init__(self, tamaño=1, tablero=10):
         self.tamaño = tamaño
         self.tablero = tablero
-     
-class catamaran():
-    def __init__(self, tamaño=2, tablero=10):
-        self.tamaño = tamaño
-        self.tablero = tablero
+    def barco_rand(self):
+        import random
+        barco_random = []
+        fila_random = random.randint(0,9)
+        columna_random = random.randint(0,9)
+        barco_random.append((fila_random,columna_random))
+        orient = random.choice(["N","S","E","O"])
 
-class rapido():
-    def __init__(self, tamaño = 3, tablero =10):
-        self.tamaño = tamaño
-        self.tablero = tablero
+        while len(barco_random) < self.tamaño:
+            if orient == "O":
+                columna_random = columna_random - 1 
+            elif orient == "E":
+                columna_random = columna_random + 1
+            elif orient == "N":
+                fila_random = fila_random - 1
+            elif orient == "S":
+                fila_random = fila_random + 1
 
-class portacontenedores():
-    def __init__(self, tamaño = 4, tablero = 10):
-        self.tamaño = tamaño
-        self.tablero = tablero
+            if fila_random not in range(self.tablero) or columna_random not in range(self.tablero):
+                fila_random = random.randint(0,9)
+                columna_random = random.randint(0,9)
+                barco_random = []
+                barco_random.append((fila_random,columna_random))
+            else:
+                barco_random.append((fila_random,columna_random))
+
+            # print(barco_random)
+        return barco_random
--- variables.py
+++ variables.py
@@ -11,18 +11,18 @@ tablero_maquina = fn.crear_tablero(10)
 vidas_jugador = 20
 vidas_maquina = 20
 #BARCOS JUGADOR
-b1_jgr = cl.lancha()
-b2_jgr = cl.lancha()
-b3_jgr = cl.lancha()
-b4_jgr = cl.lancha()
-b5_jgr = cl.catamaran()
-b6_jgr= cl.catamaran()
-b7_jgr= cl.catamaran()
-b8_jgr = cl.rapido()
-b9_jgr = cl.rapido()
-b10_jgr = cl.portacontenedores()
+b1_jgr = cl.barco()
+b2_jgr = cl.barco()
+b3_jgr = cl.barco()
+b4_jgr = cl.barco()
+b5_jgr = cl.barco(2)
+b6_jgr= cl.barco(2)
+b7_jgr= cl.barco(2)
+b8_jgr = cl.barco(3)
+b9_jgr = cl.barco(3)
+b10_jgr = cl.barco(4)
 
-barcos_jugador = [fn.barco_rand(b1_jgr.tamaño, b1_jgr.tablero), fn.barco_rand(b2_jgr.tamaño, b2_jgr.tablero), fn.barco_rand(b3_jgr.tamaño, b3_jgr.tablero), fn.barco_rand(b4_jgr.tamaño, b4_jgr.tablero), fn.barco_rand(b5_jgr.tamaño, b5_jgr.tablero), fn.barco_rand(b6_jgr.tamaño, b6_jgr.tablero), fn.barco_rand(b7_jgr.tamaño, b7_jgr.tablero), fn.barco_rand(b8_jgr.tamaño, b8_jgr.tablero), fn.barco_rand(b9_jgr.tamaño, b9_jgr.tablero), fn.barco_rand(b10_jgr.tamaño, b10_jgr.tablero)]
+barcos_jugador = [b1_jgr.barco_rand(), b2_jgr.barco_rand(), b3_jgr.barco_rand(), b4_jgr.barco_rand(), b5_jgr.barco_rand(), b6_jgr.barco_rand(), b7_jgr.barco_rand(), b8_jgr.barco_rand(), b9_jgr.barco_rand(), b10_jgr.barco_rand() ]
 
 #BARCOS MAQUINA
 barco_1_maq = fn.barco_rand(1,10)  #1ºbarco 1 eslora
@@ -37,3 +37,5 @@ barco_9_maq = fn.barco_rand(3,10)
 barco_10_maq = fn.barco_rand(4,10)  #barco 4 esloras
 
 lista_maquina = [barco_1_maq, barco_2_maq, barco_3_maq, barco_4_maq, barco_5_maq, barco_6_maq, barco_7_maq, barco_8_maq, barco_10_maq]
+
+
