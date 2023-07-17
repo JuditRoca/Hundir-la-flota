b013f7859628951d686c921356cda2f91bb44e0b
JuditRoca <roca.tovar@gmail.com>
2023-07-17 19:59:39 +0200

    movimientos

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import funciones as fn 
-import variables as vr
+from src import funciones as fn 
+from src import variables as vr
 2
 print("Bienvenido a hundir la flota! Comienza el juego.")
 print("El primer turno es del jugador. \n Debes escoger la fila y columna en la que deseas disparar. Si tocas barco vuelves a tener turno, \nsi fallas  das en agua, pasamos el turno a la maquina. Suerte!")
